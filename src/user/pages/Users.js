ced85e37f3dc5496c273acb252ea6f6ce903cd4d
Ye Paing <ye@y3p.io>
2023-02-21 20:52:27 -0500

    testing using native fetch

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -7,27 +7,38 @@ import { useHttpClient } from "../../shared/hooks/http-hook";
 import { redirect } from "react-router-dom";
 
 const Users = () => {
+	const [isLoading, setIsLoading] = useState(true);
 	const [loadedUsers, setLoadedUsers] = useState(null);
-	const { error, isLoading, sendRequest, clearError } = useHttpClient();
+	const { error, sendRequest, clearError } = useHttpClient();
 	const url = `https://${window.location.hostname}:3001/api/users`;
 	let usersData;
 
-	useEffect(() => {
-		const fetchUsers = async (url) => {
-			try {
-				usersData = await sendRequest(url);
-				console.log('\n[DEBUG] usersData:', usersData);
+	// useEffect(() => {
+	// 	const fetchUsers = async (url) => {
+	// 		try {
+	// 			usersData = await sendRequest(url);
+	// 			console.log('\n[DEBUG] usersData:', usersData);
+	//
+	// 			if (!usersData.ok) {
+	// 				throw new Error(usersData.message);
+	// 			}
+	// 		} catch (err) {
+	// 			// console.log(`Error occurred in http-client.js`);
+	// 		}
+	// 		setLoadedUsers(usersData.users);
+	// 	};
+	// 	fetchUsers(url);
+	// }, [sendRequest]);
 
-				if (!usersData.ok) {
-					throw new Error(usersData.message);
-				}
-			} catch (err) {
-				// console.log(`Error occurred in http-client.js`);
-			}
-			setLoadedUsers(usersData.users);
-		};
-		fetchUsers(url);
-	}, [sendRequest]);
+	useEffect(() => {
+		fetch(url)
+			.then(res => res.json())
+			.then(data => {
+				console.log('\n[DEBUG] Retrieved User Data::', data);
+				setLoadedUsers(data.users);
+				setIsLoading(false);
+			})
+	}, [])
 
 	return (
 		<>
